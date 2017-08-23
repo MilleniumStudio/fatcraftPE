823914dcfe1b6a278558f1b9b9a67f1f542b0211
HoverEpic <vincent_c@fat.sh>
2017-08-23 15:43:22 +0200

    add Virions build script

---
+++ virions/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#https://github.com/poggit/libasynql.git
+
+#https://github.com/Falkirks/spoondetector
+
+#wget https://github.com/poggit/poggit/raw/beta/assets/php/virion.php
+#wget https://github.com/poggit/poggit/raw/beta/assets/php/virion_stub.php
+
+#cp virion.php libasynql/libasynql
+#cp virion_stub.php libasynql/libasynql
+
+../bin/php7/bin/php -dphar.readonly=0 ../cores/BlueLight/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
+--entry virion_stub.php \
+--make libasynql/libasynql/ \
+--stub '<?php require "phar://" . __FILE__ . "/virion_stub.php"; __HALT_COMPILER();' \
+--out libasynql.phar
