bb5941f82edab03fb616083d6cf008e1784e81ba
HoverEpic <vincent_c@fat.sh>
2017-09-08 10:19:17 +0200

    [Plugin] add WIP Load balancer plugin

--- plugins/install_plugins.sh
+++ plugins/install_plugins.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-if [ ! -d "Hormones/" ]; then
-    git clone https://github.com/HoverEpic/Hormones.git
-fi
+#if [ ! -d "Hormones/" ]; then
+#    git clone https://github.com/HoverEpic/Hormones.git
+#fi
+#
+#../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
+#--make Hormones/Hormones/ \
+#--out Hormones.phar
 
 ../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
---make Hormones/Hormones/ \
---out Hormones.phar
+--make LoadBalancer/ \
+--out LoadBalancer.phar
 
 if [ ! -d "devirion/" ]; then
     git clone https://github.com/poggit/devirion.git
@@ -36,7 +40,7 @@ if [ ! -d "StatsPE/" ]; then
 fi
 
 ../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
---make EconomyS/EconomyAPI/ \
+--make StatsPE/ \
 --out StatsPE.phar
 
 ../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
