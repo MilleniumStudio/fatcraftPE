ac4e5cc92b8eb73c9931461d19839839c370c78c
HoverEpic <vincent_c@fat.sh>
2017-09-08 14:01:24 +0200

    [Plugin] add StatsPE plugin

--- plugins/install_plugins.sh
+++ plugins/install_plugins.sh
@@ -1,13 +1,6 @@
-#!/bin/bash
-#if [ ! -d "Hormones/" ]; then
-#    git clone https://github.com/HoverEpic/Hormones.git
-#fi
-#
-#../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
-#--make Hormones/Hormones/ \
-#--out Hormones.phar
+#! /bin/bash
 
-../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
+../PocketMine-MP/bin/php7/bin/php -dphar.readonly=0 ../PocketMine-DevTools/src/DevTools/ConsoleScript.php \
 --make LoadBalancer/ \
 --out LoadBalancer.phar
 
@@ -15,51 +8,27 @@ if [ ! -d "devirion/" ]; then
     git clone https://github.com/poggit/devirion.git
 fi
 
-../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
+../PocketMine-MP/bin/php7/bin/php -dphar.readonly=0 ../PocketMine-DevTools/src/DevTools/ConsoleScript.php \
 --make devirion/ \
 --out devirion.phar
 
-if [ ! -d "SimpleAuth/" ]; then
-    git clone https://github.com/HoverEpic/SimpleAuth.git
-fi
-
-../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
+../PocketMine-MP/bin/php7/bin/php -dphar.readonly=0 ../PocketMine-DevTools/src/DevTools/ConsoleScript.php \
 --make SimpleAuth/ \
 --out SimpleAuth.phar
 
-if [ ! -d "EconomyS/" ]; then
-    git clone https://github.com/HoverEpic/EconomyS.git
-fi
-
-../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
---make EconomyS/EconomyAPI/ \
---out EconomyAPI.phar
-
-if [ ! -d "StatsPE/" ]; then
-    git clone https://github.com/HoverEpic/StatsPE.git
-fi
-
-../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
+../PocketMine-MP/bin/php7/bin/php -dphar.readonly=0 ../PocketMine-DevTools/src/DevTools/ConsoleScript.php \
 --make StatsPE/ \
 --out StatsPE.phar
 
-../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
+../PocketMine-MP/bin/php7/bin/php -dphar.readonly=0 ../PocketMine-DevTools/src/DevTools/ConsoleScript.php \
 --make Worlds/ \
 --out Worlds.phar
 
-if [ ! -d "Parkour/" ]; then
-    git clone https://github.com/HoverEpic/Parkour.git
-fi
-
-../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
+../PocketMine-MP/bin/php7/bin/php -dphar.readonly=0 ../PocketMine-DevTools/src/DevTools/ConsoleScript.php \
 --make Parkour/ \
 --out Parkour.phar
 
-if [ ! -d "HungerGames-UPDATED/" ]; then
-    git clone https://github.com/HoverEpic/HungerGames-UPDATED.git
-fi
-
-../bin/php7/bin/php -dphar.readonly=0 ../cores/PocketMine-DevTools/src/DevTools/ConsoleScript.php \
+../PocketMine-MP/bin/php7/bin/php -dphar.readonly=0 ../PocketMine-DevTools/src/DevTools/ConsoleScript.php \
 --make HungerGames-UPDATED/HungerGames/ \
 --out HungerGames.phar
 
