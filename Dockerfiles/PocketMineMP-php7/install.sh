2e78804d8a4cc3ca18c85b1163c64d16eaa38d95
HoverEpic <vincent_c@fat.sh>
2017-09-13 11:54:21 +0200

    [INFRA] update configs for pocket mine MP base docker image

--- Dockerfiles/PocketMineMP-php7/install.sh
+++ Dockerfiles/PocketMineMP-php7/install.sh
@@ -8,9 +8,6 @@ cp -r template/* install/
 cp ../../cores/PocketMine-MP.phar install/
 
 cp -r ../../virions/libasynql/libasynql install/virions
-cp -r ../../virions/spoondetector/ install/virions
 
-cp ../../plugins/Hormones.phar install/plugins
+cp ../../plugins/LoadBalancer.phar install/plugins
 cp ../../plugins/devirion.phar install/plugins
-cp ../../plugins/SimpleAuth.phar install/plugins
-
