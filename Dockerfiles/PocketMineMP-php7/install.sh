3c8bbbbb5d8d9d4de3da68633c78b12b0ed38b1a
HoverEpic <vincent_c@fat.sh>
2017-09-13 14:36:56 +0200

    [INFRA] remove virions from docker image

--- Dockerfiles/PocketMineMP-php7/install.sh
+++ Dockerfiles/PocketMineMP-php7/install.sh
@@ -7,7 +7,4 @@ cp -r template/* install/
 
 cp ../../cores/PocketMine-MP.phar install/
 
-cp -r ../../virions/libasynql/libasynql install/virions
-
 cp ../../plugins/LoadBalancer.phar install/plugins
-cp ../../plugins/devirion.phar install/plugins
