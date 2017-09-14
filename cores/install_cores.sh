7e8eabffeb4269309df6485c3e1c4bdef9d30990
HoverEpic <vincent_c@fat.sh>
2017-09-14 17:45:58 +0200

    [INFRA] fix core build

--- cores/install_cores.sh
+++ cores/install_cores.sh
@@ -2,7 +2,7 @@
 
 if [ ! -e "PocketMine-MP.phar" ]; then
 ../PocketMine-MP/bin/php7/bin/php -dphar.readonly=0 ../PocketMine-DevTools/src/DevTools/ConsoleScript.php \
---make ../PocketMine-MP/src/ \
+--make ../PocketMine-MP/src,../PocketMine-MP/vendor/ \
 --relative ../PocketMine-MP/ \
 --entry src/pocketmine/PocketMine.php \
 --out PocketMine-MP.phar
