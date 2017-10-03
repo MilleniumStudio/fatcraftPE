82fc0facba0dcbb0b318a0f90fc6fedf6ba2280f
HoverEpic <vincent_c@fat.sh>
2017-10-03 10:41:58 +0200

    [INFRA] remove loadbalancer & add a lobby in start script

--- start_network.sh
+++ start_network.sh
@@ -18,10 +18,11 @@ start_docker()
 }
 
 # start front load-balancer
-start_docker lb 1 19132 fatcraft/pocketmine:lb
+#start_docker lb 1 19132 fatcraft/pocketmine:lb
 
 # start lobbies
-start_docker lobby 1 19133 fatcraft/pocketmine:lobby
+start_docker lobby 1 19132 fatcraft/pocketmine:lobby
+start_docker lobby 2 19133 fatcraft/pocketmine:lobby
 
 # start games
 start_docker hg 1 19134 fatcraft/pocketmine:hg-1
@@ -33,5 +34,5 @@ start_docker bw 1 19137 fatcraft/pocketmine:bw-1
 ## DEBUG
 #docker run --rm --name lb-1 --hostname lb-1 --env SERVER_NAME=lb-1 --env SERVER_PORT=19132 --env SERVER_TYPE=lb --env SERVER_ID=1 --publish 19132:19132 --publish 19132:19132/udp --link mysql:mysql -ti fatcraft/pocketmine:lb
 
-#docker run --rm --name lobby1 --hostname lobby1 --env SERVER_NAME=lobby1 --env SERVER_PORT=19133 --publish 19133:19133 --publish 19133:19133/udp -ti fatcraft/pocketmine:lobby
-#docker run --rm --name lobby2 --hostname lobby2 --env SERVER_NAME=lobby2 --env SERVER_PORT=19134 --publish 19134:19134 --publish 19134:19134/udp -ti fatcraft/pocketmine:lobby
+#docker run --rm --name lobby1 --hostname lobby1 --env SERVER_NAME=lobby1 --env SERVER_PORT=19132 --publish 19132:19132 --publish 19132:19132/udp --link mysql:mysql -ti fatcraft/pocketmine:lobby
+#docker run --rm --name lobby2 --hostname lobby2 --env SERVER_NAME=lobby2 --env SERVER_PORT=19134 --publish 19134:19134 --publish 19134:19134/udp --link mysql:mysql -ti fatcraft/pocketmine:lobby
