1336dd0d31d575eacf48d3fea1e4d4a714e1fb0b
HoverEpic <vincent_c@fat.sh>
2017-10-03 17:44:33 +0200

    [HungerGames] implements simple ScoresManager

--- plugins/FatcraftHungerGames/src/hungergames/score/HungerGameScoreManager.php
+++ plugins/FatcraftHungerGames/src/hungergames/score/HungerGameScoreManager.php
@@ -4,15 +4,14 @@ namespace hungergames\score;
 
 use hungergames\HungerGame;
 use pocketmine\Player;
-use fatutils\gamedata\GameDataManager;
+use fatutils\scores\ScoresManager;
 
-class HungerGameScoreManager
+class HungerGameScoreManager extends ScoresManager
 {
 
     private static $m_Instance = null;
 
     private $m_Index = 0;
-    private $m_DeathOrder = array();
 
     public static function getInstance(): HungerGameScoreManager
     {
@@ -22,65 +21,12 @@ class HungerGameScoreManager
 
     private function __construct()
     {
-
+        parent::__construct();
     }
 
     public function registerDeath(Player $p_Player)
     {
-        HungerGame::getInstance()->getLogger()->info("Register player DEATH " . $p_Player->getName() . " position " . $this->m_Index);
-        $this->m_DeathOrder[$this->m_Index] = $p_Player->getName();
-        $this->m_Index++;
-    }
-
-    public function giveRewards()
-    {
-        HungerGame::getInstance()->getLogger()->info("Giving rewards :");
-        $l_ReverseDeathOrder = array_reverse($this->m_DeathOrder);
-        var_dump($l_ReverseDeathOrder);
-
-        // record ordered players
-        GameDataManager::getInstance()->recordBoard($l_ReverseDeathOrder);
-
-        // get max rewards from config
-        $l_Rewards = HungerGame::getInstance()->getConfig()->get("rewards");
-
-        // define base divider
-        $l_Divider = 1;
-
-        // reverse death order & iter
-        foreach ($l_ReverseDeathOrder as  $l_PlayerName)
-        {
-            // get player instance
-            $p_Player = HungerGame::getInstance()->getServer()->getPlayer($l_PlayerName);
-
-            // calculate rewards (based on reverse death order)
-            $l_Money = round($l_Rewards["money"] / $l_Divider);
-            $l_XP = round($l_Rewards["money"] / $l_Divider);
-            HungerGame::getInstance()->getLogger()->info("Player " . $l_PlayerName . " win " . $l_Money . " money");
-            HungerGame::getInstance()->getLogger()->info("Player " . $l_PlayerName . " win " . $l_XP . " XP");
-
-            // check if player is online (no reward if offline)
-            if ($p_Player != null)
-            {
-                // add general stats
-                \SalmonDE\StatsPE\CustomEntries::getInstance()->modIntEntry("Money", $p_Player, $l_Money);
-                \SalmonDE\StatsPE\CustomEntries::getInstance()->modIntEntry("XP", $p_Player, $l_XP);
-
-                $p_Player->sendMessage(str_replace("{0}", $l_Money, HungerGame::getInstance()->getConfig()->get("endgameMessageMoney")));
-                $p_Player->sendMessage(str_replace("{0}", $l_XP, HungerGame::getInstance()->getConfig()->get("endgameMessageXP")));
-
-                // add game specific stats
-                $l_ServerType = \fatcraft\loadbalancer\LoadBalancer::getInstance()->getServerType();
-                \SalmonDE\StatsPE\CustomEntries::getInstance()->modIntEntry($l_ServerType . "_XP", $p_Player, $l_XP);
-                \SalmonDE\StatsPE\CustomEntries::getInstance()->modIntEntry($l_ServerType . "_played", $p_Player, 1);
-
-                // increment divider
-                $l_Divider++;
-            }
-            else
-            {
-                HungerGame::getInstance()->getLogger()->info("Player " . $l_PlayerName . " not connected, not recieve " . $l_Money . " money & " . $l_XP . " XP.");
-            }
-        }
+        HungerGame::getInstance()->getLogger()->info("Register player DEATH " . $p_Player->getName() . " position " . count($this->m_Positions));
+        $this->m_Positions[] = $p_Player->getName();
     }
 }
