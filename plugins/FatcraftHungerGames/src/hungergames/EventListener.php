d447159d6da82238bbd1a1fdfb7bce5fb02cd6ae
HoverEpic <vincent_c@fat.sh>
2017-10-10 19:07:32 +0200

    [HungerGame] fix respawn if death before game start

--- plugins/FatcraftHungerGames/src/hungergames/EventListener.php
+++ plugins/FatcraftHungerGames/src/hungergames/EventListener.php
@@ -7,10 +7,12 @@ use fatutils\players\PlayersManager;
 use fatutils\scores\PlayerScoresManager;
 use fatutils\tools\Sidebar;
 use fatutils\tools\WorldUtils;
+use fatutils\spawns\SpawnManager;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
 use pocketmine\utils\TextFormat;
 
 class EventListener implements Listener
@@ -26,28 +28,31 @@ class EventListener implements Listener
     public function playerDeathEvent(PlayerDeathEvent $e)
     {
         $p = $e->getEntity();
-        PlayerScoresManager::getInstance()->registerPlayer($p->getPlayer());
-        PlayersManager::getInstance()->getFatPlayer($p)->setHasLost(true);
-
-        WorldUtils::addStrike($p->getLocation());
-        $l_PlayerLeft = PlayersManager::getInstance()->getAlivePlayerLeft();
-
-
-        foreach (HungerGame::getInstance()->getServer()->getOnlinePlayers() as $l_Player)
+        if (!GameManager::getInstance()->isWaiting())
         {
-            $l_Player->sendMessage($e->getDeathMessage());
-            if ($l_PlayerLeft > 1)
-                $l_Player->sendMessage("Il reste " . TextFormat::YELLOW . PlayersManager::getInstance()->getAlivePlayerLeft() . TextFormat::RESET . " survivants !", "*");
+            PlayerScoresManager::getInstance()->registerPlayer($p->getPlayer());
+            PlayersManager::getInstance()->getFatPlayer($p)->setHasLost(true);
+
+            WorldUtils::addStrike($p->getLocation());
+            $l_PlayerLeft = PlayersManager::getInstance()->getAlivePlayerLeft();
+
+
+            foreach (HungerGame::getInstance()->getServer()->getOnlinePlayers() as $l_Player)
+            {
+                $l_Player->sendMessage($e->getDeathMessage());
+                if ($l_PlayerLeft > 1)
+                    $l_Player->sendMessage("Il reste " . TextFormat::YELLOW . PlayersManager::getInstance()->getAlivePlayerLeft() . TextFormat::RESET . " survivants !", "*");
+            }
+
+            if ($l_PlayerLeft <= 1 && !GameManager::getInstance()->isGameFinished())
+                HungerGame::getInstance()->endGame();
+
+            $e->setDeathMessage("");
+            $p->setGamemode(3);
+
+            Sidebar::getInstance()->update();
         }
-
-        if ($l_PlayerLeft <= 1 && !GameManager::getInstance()->isGameFinished())
-            HungerGame::getInstance()->endGame();
-
-        $e->setDeathMessage("");
-        $p->setGamemode(3);
-
-        Sidebar::getInstance()->update();
-	}
+    }
 
     /**
      * @param BlockBreakEvent $e
@@ -78,5 +83,16 @@ class EventListener implements Listener
         $p->getInventory()->clearAll();
 
         HungerGame::getInstance()->handlePlayerConnection($p);
+    }
+
+    public function onPlayerRespawn(PlayerRespawnEvent $p_Event)
+    {
+        if (GameManager::getInstance()->isWaiting())
+        {
+            $spawn = SpawnManager::getInstance()->getRandomEmptySpawn();
+            $position = \pocketmine\level\Position::fromObject($spawn->getLocation()->add(-0.5, 0.1, -0.5), $spawn->getLocation()->getLevel());
+            $p_Event->setRespawnPosition($position);
+            HungerGame::getInstance()->getLogger()->info("Player " . $p_Event->getPlayer()->getName() . " respawn at " . $position->__toString());
+        }
     }
 }
