ca475fe302f2ca48f8730b41a622fae5308f91af
HoverEpic <vincent_c@fat.sh>
2017-10-11 14:46:36 +0200

    [FatUtils] Update language, fix language change event, disable menu command

--- plugins/FatUtils/src/fatutils/events/LanguageUpdatedEvent.php
+++ plugins/FatUtils/src/fatutils/events/LanguageUpdatedEvent.php
@@ -4,12 +4,13 @@ namespace fatutils\events;
 
 class LanguageUpdatedEvent extends \pocketmine\event\player\PlayerEvent
 {
+    public static $handlerList = null;
 
     private $m_Language;
 
     public function __construct(\pocketmine\Player $Player, int $Language)
     {
-        parent::__cconstruct($Player);
+        $this->player = $Player;
         $this->m_Language = $Language;
     }
 
@@ -19,4 +20,3 @@ class LanguageUpdatedEvent extends \pocketmine\event\player\PlayerEvent
     }
 
 }
-
