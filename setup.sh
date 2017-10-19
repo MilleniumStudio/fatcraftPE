75f1ce201476059b22fd2591bd4334eae11415f6
HoverEpic <vincent_c@fat.sh>
2017-10-19 15:03:08 +0200

    [INFRA] configure setup.sh with options

--- setup.sh
+++ setup.sh
@@ -1,84 +1,74 @@
 #!/bin/bash
 
-## check if user is root
-#if [ "$(id -u)" != "0" ]; then
-#	echo "Sorry, you are not root (use sudo $command)."
-#	exit 1
-#fi
-#
-## setup mysql user & home
-#egrep -i '^mysql:' /etc/passwd
-#if [ $? -eq 0 ]; then
-#    userdel mysql
-#fi
-#
-#adduser mysql --no-create-home --system --shell /bin/sh
-#adduser mysql docker
-#
-#mkdir -p ../../data/mysql
-#chown -R mysql ../../data/mysql
-#
-#source env.sh
-#
-## TODO start/create resolver & mysql
-#docker run \
-#--name resolver \
-#--restart=always \
-#--hostname resolver \
-#-e EXCLUDE_LABEL=logspout.exclude \
-#-v /etc/localtime:/etc/localtime:ro \
-#-v /etc/timezone:/etc/timezone:ro \
-#-v /var/run/docker.sock:/tmp/docker.sock \
-#-v /etc/resolv.conf:/tmp/resolv.conf \
-#-l logspout.exclude=true \
-#-d mgood/resolvable
-#
-#docker run \
-#--user $(id mysql -u) \
-#--name mysql \
-#--restart=always \
-#--hostname mysql \
-#-e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASS \
-#-e MYSQL_USER=$MYSQL_USER \
-#-e MYSQL_PASSWORD=$MYSQL_PASS \
-#-e MYSQL_DATABASE=$MYSQL_DATA \
-#-v /etc/localtime:/etc/localtime:ro \
-#-v /etc/timezone:/etc/timezone:ro \
-#-v `pwd`/../../data/mysql:/var/lib/mysql \
-#-v `pwd`/confs/mysql:/etc/mysql/conf.d \
-#-p 3306:3306 \
-#-d mysql:5.6.33 --max_allowed_packet=10M
+UPDATE_POCKETMINE_CORE="no"
+THREADS=1
+COMPILE_BIN="no"
+BUILD_POCKETMINE_CORE="no"
+BUILD_POCKETMINE_DOCKER="no"
+
+
+while getopts "::ujcdp" OPTION; do
+
+    case $OPTION in
+        u)
+            echo "[opt] Update PocketMine-MP repository to latest version"
+            UPDATE_POCKETMINE_CORE="yes"
+            ;;
+        j)
+            echo "[opt] Set make threads to $OPTARG"
+            THREADS="$OPTARG"
+            ;;
+        c)
+            echo "[opt] PocketMine-MP build core set to YES"
+            BUILD_POCKETMINE_CORE="yes"
+            ;;
+        d)
+            echo "[opt] PocketMine-MP docker image build set to YES"
+            BUILD_POCKETMINE_DOCKER="yes"
+            ;;
+        p)
+            echo "[opt] Recompile PHP7 set to YES"
+            COMPILE_BIN="yes"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTION$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
 
 # Setup custom PocketMineMP & PocketMine-DevTools
-git submodule init
-git submodule update --recursive
-
-if [ ! -d "PocketMine-MP/bin/" ]; then
-    cd PocketMine-MP
+if [ "$UPDATE_POCKETMINE_CORE" == "yes" ]; then
     git submodule init
     git submodule update --recursive
+fi
+
+if [ "$BUILD_POCKETMINE_CORE" == "yes" ]; then
+    rm -rf core/PocketMine-MP.phar
+fi
+
+if [ "$COMPILE_BIN" == "yes" ]; then
+    cd PocketMine-MP
     ./compile.sh
     cd ../
 fi
 
 # Build server core
-cd cores/
-./install_cores.sh
-cd ../
+if [ "$BUILD_POCKETMINE_DOCKER" == "yes" ]; then
+    cd cores/
+    ./install_cores.sh
+    cd ../
+fi
 
 cd plugins/
 ./install_plugins.sh
 cd ../
 
-#cd virions/
-#./install_virions.sh
-#cd ../
-
 cd Dockerfiles/
 
-cd PocketMineMP-php7/
-./docker_build.sh
-cd ../
+#cd PocketMineMP-php7/
+#./docker_build.sh
+#cd ../
 
 #cd entry-lb/
 #./docker_build.sh
