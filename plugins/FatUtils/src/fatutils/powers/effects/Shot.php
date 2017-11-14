e1a760e3d4eef298533afe3c78b5cf89294539c8
AlexisBnt <bonnet.alexis@gmail.com>
2017-11-14 15:58:12 +0100

    Powers: Increase Shot distance from 10 to 20

--- plugins/FatUtils/src/fatutils/powers/effects/Shot.php
+++ plugins/FatUtils/src/fatutils/powers/effects/Shot.php
@@ -22,6 +22,7 @@ class Shot extends APower
     /*
      *  the ray is based on the boat yaw, cause we don't have the player's yaw when in a boat...
      */
+    const DISTANCE = 20;
 
     function getIcon(): Item
     {
@@ -34,12 +35,12 @@ class Shot extends APower
         /** @var Position $loc */
         $loc = $this->owner->vehicle->asPosition();
         $vec = new Vector3(cos(deg2rad($this->owner->vehicle->yaw)), 0, sin(deg2rad($this->owner->vehicle->yaw)));
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < self::DISTANCE; $i++) {
             $loc = Position::fromObject($loc->add($vec), $loc->level);
             ParticleBuilder::fromParticleId(Particle::TYPE_REDSTONE)->play($loc);
             $entities = $loc->level->getNearbyEntities(new AxisAlignedBB($loc->x - 0.5, $loc->y - 0.5, $loc->z - 0.5, $loc->x + 0.5, $loc->y + 0.5, $loc->z + 0.5));
             foreach ($entities as $entity) {
-                if($entity !== $this->owner && $entity !== $this->owner->vehicle) {
+                if ($entity !== $this->owner && $entity !== $this->owner->vehicle) {
                     $entity->setMotion(new Vector3(0, 1, 0));
                 }
             }
