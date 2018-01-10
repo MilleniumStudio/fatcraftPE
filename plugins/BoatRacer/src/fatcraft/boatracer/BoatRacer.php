1715615dc9478b7ad2bddc2c0dd7d92a842eaa9b
Naphtaline <le.tux.tueur@gmail.com>
2018-01-10 18:05:25 +0100

    [PLUGIN] boat racer : add reset timer and UI on waiting cancelation

--- plugins/BoatRacer/src/fatcraft/boatracer/BoatRacer.php
+++ plugins/BoatRacer/src/fatcraft/boatracer/BoatRacer.php
@@ -351,6 +351,46 @@ class BoatRacer extends PluginBase implements Listener
 		$p_Event->setCancelled(true);
 	}
 
+	private function resetGameWaiting()
+	{
+		// Waiting Clock Initialization
+		$this->m_WaitingTimer = new DisplayableTimer(GameManager::getInstance()->getWaitingTickDuration());
+		$this->m_WaitingTimer
+			->setTitle(new TextFormatter("timer.waiting.title"))
+			->addStopCallback(function ()
+			{
+				$this->startGame();
+			})
+			->addSecondCallback(function () {
+				if ($this->m_WaitingTimer instanceof Timer)
+				{
+					$l_SecLeft = $this->m_WaitingTimer->getSecondLeft();
+					$l_Text = "";
+					if ($l_SecLeft == 3)
+						$l_Text = TextFormat::RED . $l_SecLeft;
+					else if ($l_SecLeft == 2)
+						$l_Text = TextFormat::GOLD . $l_SecLeft;
+					else if ($l_SecLeft == 1)
+						$l_Text = TextFormat::YELLOW . $l_SecLeft;
+
+					foreach (FatUtils::getInstance()->getServer()->getOnlinePlayers() as $l_Player)
+						$l_Player->addTitle($l_Text, "");
+				}
+			});
+
+		Sidebar::getInstance()->clearLines();
+		// Waiting Sidebar Initialization
+		Sidebar::getInstance()
+			->addTranslatedLine(new TextFormatter("template.br"))
+			->addTimer($this->m_WaitingTimer)
+			->addWhiteSpace()
+			->addMutableLine(function ()
+			{
+				return new TextFormatter("game.waitingForMore", ["amount" => max(0, PlayersManager::getInstance()->getMinPlayer() - count($this->getServer()->getOnlinePlayers()))]);
+			});
+		Sidebar::getInstance()->update();
+	}
+
 	public function onPlayerQuit(PlayerQuitEvent $p_Event)
 	{
 		if (GameManager::getInstance()->isPlaying())
@@ -372,6 +412,7 @@ class BoatRacer extends PluginBase implements Listener
 				{
 					$this->m_WaitingTimer->cancel();
 					$this->m_WaitingTimer = null;
+					$this->resetGameWaiting();
 				}
 			} else if (GameManager::getInstance()->isPlaying())
 			{
